9e0a1beacf5f5cb4641d22b996c0e8fe85caf8e3
sergeykochiev <sergeyivanovo547@gmail.com>
2024-04-15 01:30:02 +0300

    dupa1

--- src/app/(user)/channels/[slug]/(1)/tasks/page.tsx
+++ src/app/(user)/channels/[slug]/(1)/tasks/page.tsx
@@ -7,6 +7,13 @@ import NoDataPlaceholder from "@/components/NoDataPlaceholder";
 import TaskTile from "@/components/Tile/Task";
 import { useState } from "react";
 
+export async function generateStaticParams() {
+    return CHANNELS.map((channel) => ({
+      slug: channel.slug,
+    }))
+  }
+
+
 export default function Page() {
     // const [search, setSearch] = useState<string>("")
     const channelTasks: Array<TaskStruct> = TASKS.slice(0,4)
