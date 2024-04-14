fa4c7e69a316c1d95b1e89f7e0b78171f32806fc
sergeykochiev <sergeyivanovo547@gmail.com>
2024-04-15 01:28:53 +0300

    dupa

--- src/app/(user)/channels/[slug]/(1)/tasks/page.tsx
+++ src/app/(user)/channels/[slug]/(1)/tasks/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { CHANNELS, TASKS } from "@/common/temp-data";
 import TaskStruct from "@/common/types/data-sctructures/task";
 import ContentContainer from "@/components/ContentContainer";
@@ -5,20 +7,9 @@ import NoDataPlaceholder from "@/components/NoDataPlaceholder";
 import TaskTile from "@/components/Tile/Task";
 import { useState } from "react";
 
-export async function generateStaticParams() {
-    return CHANNELS.map((channel) => ({
-      slug: channel.slug,
-    }))
-  }
-
-
-export default function Page({ params }: { params: { slug: string }}) {
-    const currentChannel = CHANNELS.find(e => e.slug == params.slug)
-    if (!currentChannel) {
-        return <NoDataPlaceholder/>
-    }
+export default function Page() {
     // const [search, setSearch] = useState<string>("")
-    const channelTasks: Array<TaskStruct> = TASKS.filter(e => e.channelId == currentChannel.id)
+    const channelTasks: Array<TaskStruct> = TASKS.slice(0,4)
     return (
         <ContentContainer data={channelTasks} Component={TaskTile}/>
     )   
