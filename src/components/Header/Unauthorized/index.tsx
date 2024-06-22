c33f06cce1202c703f88e22fc8d5e8cfe48d7907
sergeykochiev <sergeyivanovo547@gmail.com>
2024-06-22 13:41:32 +0300

    added x padding to header

--- src/components/Header/Unauthorized/index.tsx
+++ src/components/Header/Unauthorized/index.tsx
@@ -2,7 +2,7 @@ import { sourceCodePro } from "@/common/fonts";
 import HeaderTab from "../HeaderTab";
 
 export function UnauthHeader() {
-    return <header className={`${sourceCodePro.className} h-[57px] w-full bg-tt-white flex justify-center`}>
+    return <header className={`${sourceCodePro.className} h-[57px] px-[24px] w-full bg-tt-white flex justify-center`}>
         <div className="flex justify-between items-center w-[1920px]">
             <div className="uppercase text-subtitle font-extrabold text-tt-blue">Task Tycoon</div>
             <div className="flex gap-big">
